5072072701d061b3fb7bce79232fe822058d9d99
HerohollandYT <hollandjackson@gmail.com>
2020-10-22 15:24:20 -0500

    Created the app.

---
+++ WordCount/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Net;
+
+namespace WordCount {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Please enter your txt file path!");
+            string filepath = Console.ReadLine();
+            string document;
+            if (filepath == "default")
+            {
+                document = File.ReadAllText("C:/Users/heroh/Desktop/draft2.txt");
+            }
+            else
+            {
+                document = File.ReadAllText(filepath);
+            }
+            string[] docdataraw = document.Split(' ');
+            Dictionary<string, int> wordcounts = new Dictionary<string,int>();
+            for (int i = 0; i < docdataraw.Length; i++)
+            {
+                    if (!wordcounts.ContainsKey(docdataraw[i]))
+                    {
+                        wordcounts.Add(docdataraw[i], 1);
+                    }
+                    else
+                    {
+                        wordcounts[docdataraw[i]] = wordcounts[docdataraw[i]] + 1;
+                    }
+            }
+
+            Console.WriteLine("Here are some words to consider changing.");
+            foreach (var word in wordcounts)
+            {
+                if (word.Value > 1)
+                {
+                    Console.WriteLine(word.Key + ": " + word.Value);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
